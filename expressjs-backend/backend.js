87243f884d962f930d348954f775103a57c14807
Andrea Salazar Santos <45272203+dreassantos@users.noreply.github.com>
2023-04-16 23:07:55 -0700

    added users jSON object

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -25,3 +25,41 @@ app.get('/', (req, res) =>{
 app.listen(port, () => { 
 	console.log(`Example app listening at http://localhost:${port}`); 
 });
+
+//Use get to define a rout called '/users' which will return the entire list of users. 
+app.get('/users', (req, res) => {
+	res.send(users);
+});
+
+
+//JSON Object of users
+const users = { 
+   users_list :
+   [
+      { 
+         id : 'xyz789',
+         name : 'Charlie',
+         job: 'Janitor',
+      },
+      {
+         id : 'abc123', 
+         name: 'Mac',
+         job: 'Bouncer',
+      },
+      {
+         id : 'ppp222', 
+         name: 'Mac',
+         job: 'Professor',
+      }, 
+      {
+         id: 'yat999', 
+         name: 'Dee',
+         job: 'Aspring actress',
+      },
+      {
+         id: 'zap555', 
+         name: 'Dennis',
+         job: 'Bartender',
+      }
+   ]
+}
